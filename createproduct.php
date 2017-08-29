efc0dd05c40f40a6df173d0af038404bad5dd0aa
aishwaryap123 <aishwaryapratpsingh@cedcoss.com>
2017-08-29 20:40:27 +0530

    functions

--- createproduct.php
+++ createproduct.php
@@ -1,55 +1,92 @@
 <?php include('header.php'); ?>
+<?php include "../functions.php"; ?>
+<?php include "../config.php";?>
 		<?php $page=basename($_SERVER['PHP_SELF']); ?>
 		<?php include('sidebar.php'); ?>
 		<?php
-			include "config.php";
+			
+			$tag="";
+		    if(isset($_POST['tag1'])){
+		    	$tag=$_POST['tag1'];
+		    }
+		    else if(isset($_POST['tag2'])){
+		    	$tag=$_POST['tag2'];
+		    }
 			if(isset($_POST['submit'])){
 			
 			$name=$_POST['product-name'];
 			$price=$_POST['product-price'];
 			$category=$_POST['dropdown'];
 			$image="";
-
 		}
+		//to show edit field in form
 		if(isset($_GET['e_id'])){
 			$edt_id=$_GET['e_id'];
 			$stmt=$conn->prepare("SELECT * FROM  PRODUCT WHERE id=?");
 			$stmt->bind_param("i",$edt_id);
-			$stmt->bind_result($id1,$name1,$price1,$image1,$category1);
+			$stmt->bind_result($id1,$name1,$price1,$image1,$category1,$tag1);
 			$stmt->execute();
 			                    while($stmt->fetch()){
 										$i=$id1;
 										$n=$name1;
 										$p=$price1;
 										$img=$image1;
+										$tag_edit=$tag1;
 									}
+			$stmt->close();						
 		}
+		//to update price and name
 		if(isset($_POST['edit'])){
 			$postId=$_POST['edit'];
 			$postName=$_POST['product-name'];
 			$postPrice=$_POST['product-price'];
-			$stmt = $conn->prepare("UPDATE PRODUCT SET name=?,price=? WHERE id=?");
-				$stmt->bind_param("ssi", $postName,$postPrice,$postId);
+			$sql="UPDATE PRODUCT SET name=?,price=?";
+			if(isset($_FILES['image'])){
+
+				
+
+
+				if(move_uploaded_file($_FILES['image']['tmp_name'],"../uploads/".$_FILES['image']['name'])){
+					$filename = $_FILES['image']['name'];
+					$postImage=$filename;
+				}
+				$sql.=", image=? WHERE id=?";
+			
+			$stmt = $conn->prepare($sql);
+				$stmt->bind_param("sssi", $postName,$postPrice,$postImage,$postId);
 					$stmt->execute();
 						$stmt->close();
 							//header("location:dlt.php");
 		}
+		else{
+			$sql="WHERE id=?";
+				$stmt = $conn->prepare($sql);
+					$stmt->bind_param("ssi", $postName,$postPrice,$postId);
+						$stmt->execute();
+						$stmt->close();
+
+		}
+	}
+		//to uploads image files....
 		if(isset($_POST['submit'])){
 		
 			$filename="";
 			if(isset($_FILES['image'])){
 					
-					if(move_uploaded_file($_FILES['image']['tmp_name'],"../uploads/".$_FILES['image']['name'])){
-							$filename = $_FILES['image']['name'];
-								$image=$filename;
+				if(move_uploaded_file($_FILES['image']['tmp_name'],"../uploads/".$_FILES['image']['name'])){
+						$filename = $_FILES['image']['name'];
+							$image=$filename;
+				$product=array('name'=>$name,'price'=>$price,'category'=>$category,'image'=>$image,'tag'=>$tag);
+					print_r($product);
+						$exe=addProduct($product); //to insert products....
+		
 			 }
 			}
-
 		}
-		$stmt=$conn->prepare("INSERT INTO PRODUCT (name,price,image,category) values (?,?,?,?)");
-				$stmt->bind_param("ssss",$name,$price,$image,$category);
-					$stmt->execute();
-						$stmt->close();
+		
+		
+		//to insert category in dropdown...
+		$ctg=showCategory();
 
 		?>
 
@@ -75,7 +112,15 @@
 						<form action="#" method="post" enctype="multipart/form-data">
 							
 							<fieldset> <!-- Set class to "column-left" or "column-right" on fieldsets to divide the form into columns -->
-								
+								<p><?php if(isset($exe)): ?>
+								<label>Small form input</label>
+										<input class="text-input small-input" type="text" id="small-input" name="small-input" /> <span class="input-notification success png_bg">
+										Product added Successfully </span>
+
+								<?php endif ?>
+
+									
+								</p>
 								
 								
 								<p>
@@ -90,7 +135,7 @@
 								<p>
 								<label for="product_image">
 								<span>Product Image</span> 
-								<input type="file" name="image" value="upload_image" id="imageToupload">
+								<input type="file" name="image" value="<?php if(isset($_GET['e_id'])){ echo $img;}?>" id="imageToupload">
 				
 								</label>
 								</p>
@@ -104,15 +149,18 @@
 									<label>This is a drop down list</label>              
 									<select name="dropdown" class="small-input">
 										<option name="option0" value="option0">Category</option>
-										<option name="option1" value="Sports">Sports</option>
-										<option  name="option2" value="Electronics">Elctronics</option>
-										
-									</select> 
-								</p>
+								<?php foreach($ctg as $x=>$y):?>		
+								<option  value="<?php echo $y['name']; ?>"><?php
+								 echo $y['name']?></option>
+								
+								<?php endforeach; ?>
+							</select> 
+						</p>
 								
 								<p>
-									<label>Textarea with WYSIWYG</label>
-									<textarea class="text-input textarea wysiwyg" id="textarea" name="textfield" cols="79" rows="15"></textarea>
+									<label>Product Tags</label>
+									<input type="checkbox" value="brittle" class="tags" name="tag1">Brittle</input>
+									<input type="checkbox" value="fragile" class="tags" name="tag2">Fragile</input> 
 								</p>
 								
 								<p>
@@ -130,39 +178,3 @@
 						</form>
 						
 					</div> <!-- End #tab2 -->   
-
-
-
-
-
-
-
-
-
-			
-
-			
-				</div>
-			
-
-
-
-			
-			
-					
-					
-
-
-			
-			
-			
-			
-			
-			<div class="clear"></div>
-			
-			
-			
-			
-			
-			
-			<?php include('footer.php'); ?>
